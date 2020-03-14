286a1f938cbbd55833dc859a94d0f7150509f99a
Charan <sreecharan999@hotmail.com>
2020-03-14 18:19:13 +0400

    Final commit fixed the second click red background not showing up

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,21 @@
 import React, {useEffect, useRef} from "react";
 import { Link } from "react-router-dom";
 import gsap from "gsap";
-import { CSSPlugin } from 'gsap/CSSPlugin'
+
+import dallas from "../images/dallas.webp";
+import austin from "../images/austin.webp";
+import newyork from "../images/newyork.webp";
+import sanfrancisco from "../images/sanfrancisco.webp";
+import beijing from "../images/beijing.webp";
+
+
+const cities =[
+  {name: "Dallas", image: dallas},
+  {name: "Austin", image: austin},
+  {name: "New York", image: newyork},
+  {name: "San Francisco", image: sanfrancisco},
+  {name: "Beijing", image: beijing}
+];
 
 const Hamburger = ({state}) => {
 // vars for our dom nodes
@@ -17,20 +31,13 @@ const Hamburger = ({state}) => {
 useEffect(()=>{
   if(state.clicked === false){
     // close our menu
-    
-    gsap.to(menu,{
-      duration: 1,
-      css: {
-        display: "none"
-      }
-
-    });
-  }else if(state.clicked === true || (state.clicked === true && state.initial === null))
+    staggerRevealClose(revealMenu, revealMenuBackground);
+    gsap.to(menu,{duration: 1,css: {display: "none"}});
+  } else if(state.clicked === true || (state.clicked === true && state.initial === null))
   {
     // open our menu
     gsap.to(menu,{
       duration: 0,
-      opacity: "1",
       css: {display: "block"}
     });
     gsap.to([revealMenu, revealMenuBackground], {
@@ -48,20 +55,19 @@ useEffect(()=>{
 
 
 const staggerReveal = (node1, node2) =>{
-  gsap.registerPlugin(CSSPlugin)
   gsap.from([node1, node2],{
     duration: .8,
-    height: 0,
+    height: "0%",
     transformOrigin: "right top",
     skewY: 2,
     ease: "power3.inOut",
     stagger:{
-      amount: 0.3
+      amount: 0.1
     }
   });
 }
+
 const staggerText = (node1, node2, node3) =>{
-  gsap.registerPlugin(CSSPlugin);
   gsap.from([node1, node2, node3],{
   duration: .8,
    y:100,
@@ -73,8 +79,18 @@ const staggerText = (node1, node2, node3) =>{
     
   });
 }
+
+const staggerRevealClose = (node1, node2) => {
+  gsap.to([node1, node2], {
+    duration: 0.8,
+    height: "0%",
+    ease: "power3.inOut",
+    stagger: {
+      amount: 0.07
+    }
+  });
+};
 const fadeInUp = (node) =>{
-  gsap.registerPlugin(CSSPlugin)
   gsap.from(node,{
   y:60,
   duration: 1,
@@ -84,11 +100,54 @@ const fadeInUp = (node) =>{
   });
 }
 
+
+
+const handleCity = city =>{
+  gsap.to(cityBackground,{
+    duration: 0,
+    background: `url(${city}) center center`
+  });
+  gsap.to(cityBackground,{
+    duration: 0.4,
+    opacity: 1,
+    ease: "power3.inOut"
+  });
+  gsap.from(cityBackground,{
+    duration: 0.4,
+    skewY: 2,
+    transformOrigin: "right top"
+  });
+};
+
+const handleCityReturn = ()=>{
+  gsap.to(cityBackground,{
+    duration: 0.4,
+    opacity: 0
+  });
+}
+
+
+const handleHover = e =>{
+  gsap.to(e.target,{
+    duration: .3,
+    y: 3,
+    skewX: 4,
+    ease: "power#.inOut"
+  });
+}
+const handleHoverExit = e =>{
+  gsap.to(e.target,{
+    duration: .3,
+    y: -3,
+    skewX: 0,
+    ease: "power#.inOut"
+  });
+}
   return (
   <div ref={el => (menu = el)} className='hamburger-menu'>
     <div ref={el => (revealMenuBackground = el)} className="menu-secondary-background-color">
       <div ref={el => (revealMenu = el)} className="menu-layer">
-        <div className="menu-city-background">
+        <div ref={el => (cityBackground = el)}className="menu-city-background">
 
         </div>
         <div className="container">
@@ -96,9 +155,9 @@ const fadeInUp = (node) =>{
             <div className="menu-links">
               <nav>
                 <ul>
-                  <li><Link ref={el => (line1 = el)} to="/opportunities">Opportunities</Link></li>
-                  <li><Link ref={el => (line2 = el)} to="/solutions">Solutions</Link></li>
-                  <li><Link ref={el => (line3 = el)} to="/contact-us">Contact us</Link></li>
+                  <li><Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} ref={el => (line1 = el)} to="/opportunities">Opportunities</Link></li>
+                  <li><Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} ref={el => (line2 = el)} to="/solutions">Solutions</Link></li>
+                  <li><Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} ref={el => (line3 = el)} to="/contact-us">Contact us</Link></li>
                 </ul>
               </nav>
               <div ref={el => (info = el)}className="info">
@@ -107,11 +166,9 @@ const fadeInUp = (node) =>{
               </div>
               <div className="locations">
                 Locations: 
-                <span>Dallas</span>
-                <span>Austin</span>
-                <span>New york</span>
-                <span>San Francisco</span>
-                <span>Beijing</span>
+                {cities.map(el =>(
+                  <span key={el.name} onMouseEnter={()=> handleCity(el.image, cityBackground)} onMouseOut={() => handleCityReturn(cityBackground)} > {el.name}</span>
+                ))}
               </div>
             </div>
           </div>
@@ -119,6 +176,7 @@ const fadeInUp = (node) =>{
       </div>
     </div>
   </div>
-  )};
+  );
+};
 
 export default Hamburger;
